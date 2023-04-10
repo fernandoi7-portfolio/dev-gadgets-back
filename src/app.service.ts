64eaa18df0572beb4c17f5550690b2da73d0f1b3
Afonso Barreto <fernando.analista.si@gmail.com>
2023-04-09 19:38:27 -0700

    :card_file_box: Create the initial config of database
    - Integrates with prisma to connect to database \

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getHello(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
